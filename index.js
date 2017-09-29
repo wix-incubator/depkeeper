3dfb78076533e75c1f273ef848de5b3e8547b0d0
Tomas Miliauskas <tomas@wix.com>
2017-09-29 17:19:27 +0300

    feat(check-outdated): list outdated deps

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 'use strict';
 
+const semver = require('semver');
 const deps = require('./lib/deps');
+const createRegistry = require('./lib/registry');
 
-function depkeeper({cwd = process.cwd()} = {}) {
+function depkeeper({cwd = process.cwd(), registryUrl = 'https://registry.npmjs.org/'} = {}) {
+  const registry = createRegistry(registryUrl);
 
   function check(/*rules*/) {
     return deps.collect(cwd)
-      .then(list => {
-        return list;
-      });
+      .then(appendLatest)
+      .then(filterOutdated);
+  }
+
+  function appendLatest(list) {
+    return Promise.all(list.map(withLatest));
+  }
+
+  function filterOutdated(list) {
+    return list.filter(({version, latest}) =>
+      !version || !latest || semver.neq(version, latest));
+  }
+
+  function withLatest(dep) {
+    return registry.getLatest(dep.name)
+      .then(latest => Object.assign({}, dep, {latest}));
   }
 
   return {check};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,33 +1,72 @@
 const {expect} = require('chai');
 const dk = require('..');
+const shmock = require('shmock');
 const tp = require('./helpers/test-phases');
 
-describe('depkeeper', () => {
-  let test;
 
-  beforeEach(() => {
-    test = tp.create();
+describe('depkeeper', () => {
+  const port = 3333;
+  const registryUrl = `http://localhost:${port}/`;
+  const test = tp.create();
+  const npmServer = shmock(port);
+
+  afterEach(() => {
+    test.teardown();
+    npmServer.clean();
   });
 
-  afterEach(() => test.teardown());
-
   describe('.check()', () => {
-    it('should list dependencies', () => {
+    it('should return the list of outdated dependencies', () => {
       const {tmp} = test.setup({
         'node_modules/dep1/package.json': createPackage('dep1', '4.0.13'),
-        'node_modules/dep2/package.json': createPackage('dep2', '1.5.8')
+        'node_modules/dep2/package.json': createPackage('dep2', '1.5.8'),
+        'package.json': createJSON(withDeps({dep1: '', dep2: ''}))
       });
 
-      return dk({cwd: tmp}).check().then(deps => {
-        expect(deps).to.deep.equal([
-          {name: 'dep1', version: '4.0.13'},
-          {name: 'dep2', version: '1.5.8'}
-        ]);
+      mockDependencyMeta('dep1', '5.0.1');
+      mockDependencyMeta('dep2', '1.5.8');
+
+      return dk({cwd: tmp, registryUrl}).check().then(outdated =>
+        expect(outdated).to.deep.equal([{name: 'dep1', version: '4.0.13', latest: '5.0.1'}]));
+    });
+
+    it('should do nothing when there are no dependencies in package.json', () => {
+      const {tmp} = test.setup({
+        'package.json': createJSON({})
+      });
+
+      return dk({cwd: tmp, registryUrl}).check().then(outdated =>
+        expect(outdated).to.deep.equal([]));
+    });
+
+    it.skip('should use version from the package.json when there is no dependency on the disk', () => {
+      // TODO: implement
+    });
+
+    it.skip('should check deps for itself', () => {
+      return dk().check().then(outdated => {
+        console.log(outdated);
       });
     });
   });
-});
 
-function createPackage(name, version) {
-  return JSON.stringify({name, version}, null, 2);
-}
+  function createPackage(name, version) {
+    return createJSON({name, version});
+  }
+
+  function withDeps(dependencies) {
+    return {dependencies};
+  }
+
+  function createJSON(json) {
+    return JSON.stringify(json, null, 2);
+  }
+
+  function mockDependencyMeta(dep, latest) {
+    npmServer.get(`/${dep}`).reply(200, {
+      _id: dep,
+      name: dep,
+      'dist-tags': {latest}
+    });
+  }
+});
